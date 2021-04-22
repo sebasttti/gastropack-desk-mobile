e145f6202995ba823b441ee45115adc069c4c37e
sebasttti <juansjoya_r@hotmail.com>
2021-04-21 21:05:19 -0500

    initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +7,8 @@ import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './material/material.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,9 +19,10 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     CoreModule,
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    IonicModule.forRoot({_forceStatusbarPadding:true})
   ],
-  providers: [Title],
+  providers: [Title,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
